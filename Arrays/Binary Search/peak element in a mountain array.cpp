4a38ca2738e6915d097768a565a4ceca809622b0
Anurag Kumar Singh <86841935+Anuragkr07@users.noreply.github.com>
2022-09-03 19:32:59 +0530

    Update peak element in a mountain array.cpp

--- Arrays/Binary Search/peak element in a mountain array.cpp
+++ Arrays/Binary Search/peak element in a mountain array.cpp
@@ -1,25 +1,40 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-int peakIndexInMountainArray(vector<int>& arr) {
-        int s=0,e=arr.size()-1;
-        while(s<=e)
-        {
-            int mid=e+(s-e)/2;
-            if(arr[mid]>arr[mid+1] && arr[mid]>arr[mid-1])
+int search(vector<int>arr, int n, int low, int high){
+        int mid=(low+high)/2;
+        if(mid>0 && mid<n-1){
+            if(arr[mid]>arr[mid-1] && arr[mid]>arr[mid+1]){
                 return mid;
-            else if(arr[mid]<arr[mid+1])
-                s=mid+1;
-            else
-                e=mid-1;
+            }else if(arr[mid-1]>arr[mid+1]){
+                return search(arr, n, low, mid-1);
+            }else{
+                return search(arr, n, mid+1, high);
+            }
+        }else if(mid>0){
+            if(arr[mid]>arr[mid-1]){
+                return mid;
+            }else{
+                return mid-1;
+            }
+        }else if(mid<n-1){
+            if(arr[mid]>arr[mid+1]){
+                return mid;
+            }else{
+                return mid+1;
+            }
         }
-        return -1;
+        return 0;
+    }
+
+int findPeakElement(vector<int>& nums) {
+        return search(nums, nums.size(), 0, nums.size()-1);
     }
 
 int main()
 {
     vector<int>arr={0,2,1,0};
-    cout<<"Peak element is: "<<arr[peakIndexInMountainArray(arr)];
+    cout<<"Peak element is: "<<arr[findPeakElement(arr)];
     
     return 0;
 }
