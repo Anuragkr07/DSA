79f4bb8d9d286377d4fdf93f3b5494ebfe4aad48
Anurag Kumar Singh <86841935+Anuragkr07@users.noreply.github.com>
2022-10-02 07:49:03 +0530

    Add files via upload

---
+++ Binary Trees/Search in a BST.cpp
@@ -0,0 +1,27 @@
+// Problem Link : https://leetcode.com/problems/search-in-a-binary-search-tree/description/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution {
+public:
+    TreeNode* searchBST(TreeNode* root, int val) {
+        if(!root || root->val==val)
+        return root;
+
+        else if(root->val<val)
+        return searchBST(root->right,val);
+
+        else
+        return searchBST(root->left,val);
+    }
+};
