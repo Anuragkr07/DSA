f6f6e416e682846a0bd42871bdf2e0d848148cf7
Anurag Kumar Singh <86841935+Anuragkr07@users.noreply.github.com>
2022-10-02 10:15:41 +0530

    Add files via upload

---
+++ Binary Search Trees/LCA in BST.cpp
@@ -0,0 +1,20 @@
+// Problem Link : https://leetcode.com/problems/lowest-common-ancestor-of-a-binary-search-tree/description/
+
+#include<bits/stdc++.h>
+using namespace std;
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        if(!root)   return NULL;
+        if(root->val> p->val && root->val> q->val)
+            return lowestCommonAncestor(root->left,p,q);
+        else if(root->val< p->val && root->val< q->val)
+            return lowestCommonAncestor(root->right,p,q);
+        else
+        return root; 
+    }
